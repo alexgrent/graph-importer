2e5d62d706dc0a475dda9e2f6124df8f33aa5f63
alexgrent <51237839+alexgrent@users.noreply.github.com>
2024-12-16 16:36:08 +0000

    Update release_github.sh

--- release_github.sh
+++ release_github.sh
@@ -1,2 +1,10 @@
 VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+# Create a Git tag with the version
+git tag -a "$VERSION" -m "Release $VERSION"
+
+# Push the tag to the remote repository
+git push origin "$VERSION"
+
+# Create a GitHub release with the version tag
 gh release create "$VERSION" --title "Version Update" --notes "New Version Release"
